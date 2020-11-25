925fca9d1ef7e76ea5e18db11ea6c9dd7f7c5a74
hello-xiao <1276453570@qq.com>
2020-11-25 23:07:59 +0800

    登录和注册的初步完成

---
+++ resources/views/login.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.default')
+@section('content')
+    <form action="{{route('login')}}" method="post">
+        @csrf
+        <div class="card">
+            <div class="card-header">用户登录</div>
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="">邮箱</label>
+                    <input type="email" name="email" id="" class="form-control" value="{{old('email')}}">
+                </div><div class="form-group">
+                    <label for="">密码</label>
+                    <input type="password" name="password" id="" class="form-control">
+            </div>
+            <div class="card-footer text-muted">
+                <button type="submit" class="btn btn-success">登录</button>
+            </div>
+        </div>
+    </form>
+@endsection
